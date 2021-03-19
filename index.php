0bb8a89b3bfc945e09e9f218572a8d6fd2a2fcce
srikurniawanansori <srikurniawanansori@gmail.com>
2021-03-19 16:54:19 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
 <h1>hello world</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In doloremque illo delectus odit ea iusto facere iure hic quam reprehenderit, dicta iste, unde ab enim minus aliquam voluptatem nisi. Ipsa.</p>
 </body>
 </html>
